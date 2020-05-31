c4c62e3a0e104f3918883f2adfbfb9c96809c1dd
Iain R. Learmonth <irl@hambsd.org>
2020-05-31 12:53:38 +0000

    fixup aprsisd(8)

--- usr.sbin/aprsisd/aprsis.h
+++ usr.sbin/aprsisd/aprsis.h
@@ -9,20 +9,3 @@ extern int bidir;
  * for CR/LF. */
 #define TNC2_MAXLINE 510
 
-/* The following macros provide pointers to structures inside
- * AX.25 packets:
- *  p is the pointer to the start of the packet
- *  n is the number of digipeater hops plus 2 */
-#define AX25_ADDR_PTR(p, n)	 ((struct ax25_addr *)(&p[AX25_ADDR_LEN * n]))
-#define AX25_CTRL(p, n)		 (p[(AX25_ADDR_LEN * (n + 1))])
-#define AX25_PID(p, n)		 (p[(AX25_ADDR_LEN * (n + 1)) + 1])
-#define AX25_INFO_PTR(p, n)	 (&p[(AX25_ADDR_LEN * (n + 1)) + 2])
-
-struct sockaddr_ax25 {
-	u_int8_t		sax_len;
-	sa_family_t		sax_family;
-	struct ax25_addr	sax_addr;
-	int8_t			sax_pathlen;
-	struct ax25_addr	sax_path[AX25_MAX_DIGIS];
-};
-
